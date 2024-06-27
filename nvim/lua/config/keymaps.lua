44fac9a1052094fa9253b3da978df90e0e316a94
Gassyrbek Kosherbay <gas@Gassyrbeks-MacBook-Pro.local>
2024-06-28 04:32:49 +0500

    update: treesitter context
    Added the plugin called "Treesitter Context"

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,5 +5,4 @@ local keymap = vim.keymap
 keymap.set({ "n", "v" }, ";", ":")
 keymap.set("i", "jp", "<Esc>", { noremap = true })
 keymap.set("n", "<S-q>", ":bd<CR>", { noremap = true, silent = true })
-
 keymap.set("n", "<leader>we", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
